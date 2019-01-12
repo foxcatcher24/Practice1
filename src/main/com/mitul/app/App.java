d548c64b141c5124f10bc85d9965d8a764a6b7eb
foxhunter24 <tmitul515@gmail.com>
2019-01-12 20:00:33 +0600

    This is my third commit, just a simple comment.

---
+++ src/main/com/mitul/app/App.java
@@ -0,0 +1,20 @@
+package main.com.mitul.app;
+
+public class App {
+    private final String message = "Hello World!";
+
+    // Constructor
+    public App(){}
+
+    // Main Function
+    public static void main(String[] args){
+        System.out.println(new App().getMessage());
+    }
+
+    // Normal Function
+    // Returning a message
+    public final String getMessage(){
+        return message;
+    }
+
+}
---
+++ src/test/com/mitul/app/AppTest.java
@@ -0,0 +1,5 @@
+package test.com.mitul.app;
+
+public class AppTest {
+
+}
